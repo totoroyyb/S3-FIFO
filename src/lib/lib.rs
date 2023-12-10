c1138ebbbbfd9f3fd4560cd7799f7f6decfbd444
Yibo Yan <totoroq@outlook.com>
2023-12-11 00:32:19 +0800

    mod: add basic tests for RingBuffer

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,18 +1,3 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
-
 use std::fmt::{self, Debug};
 
 static DEFAULT_RINGBUF_SIZE: usize = 100;
@@ -140,19 +125,34 @@ impl<T> RingBuffer<T> {
     fn index_backword(&self, orig_index: usize) -> usize {
         self.get_index(orig_index, -1)
     }
+
+    fn get_values(&self) -> Vec<T>
+    where T: Clone 
+    {
+        let mut values = Vec::<T>::new();
+        let mut i = self.head;
+
+        // Avoid the case where the buffer is full and head == tail in this case.
+        if self.size == 0 {
+            return values;
+        }
+
+        values.push(self.buffer[i].clone());
+        i = self.index_forward(i);
+        while i != self.tail {
+            values.push(self.buffer[i].clone());
+            i = self.index_forward(i);
+        }
+        values
+    }
 }
 
 impl<T> fmt::Debug for RingBuffer<T>
 where T: Clone + Debug
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut organized_arr = Vec::<T>::new();
+        let organized_arr = self.get_values();
         // TODO: implement a custom iterator
-        let mut i = self.head;
-        while i != self.tail {
-           organized_arr.push(self.buffer[i].clone());
-           i = self.index_forward(i);
-        }
 
         f.debug_struct("RingBuffer")
             .field("elements", &organized_arr)
@@ -172,3 +172,162 @@ where T: Default + Clone
     } 
 }
 
+#[cfg(test)]
+mod ring_buffer_tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        let rb = RingBuffer::<usize>::new(5);
+        assert_eq!(rb.capacity, 5);
+        assert_eq!(rb.head, 0);
+        assert_eq!(rb.tail, 0);
+        assert_eq!(rb.size, 0);
+    }
+
+    #[test]
+    fn default_init() {
+        let rb = RingBuffer::<usize>::default();
+        assert_eq!(rb.capacity, DEFAULT_RINGBUF_SIZE);
+    }
+
+    #[test]
+    fn push_back() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE / 2) {
+            rb.push_back(i);
+            expected.push(i);
+        }
+        let result = rb.get_values();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn push_front() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE / 2) {
+            rb.push_front(i);
+            expected.push(i);
+        }
+        expected.reverse();
+        let result = rb.get_values();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn push_pop_back() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE / 2) {
+            rb.push_back(i);
+            expected.push(i);
+        }
+        for _ in 0..(DEFAULT_RINGBUF_SIZE / 4) {
+            rb.pop_back();
+            expected.pop();
+        }
+
+        let result = rb.get_values();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn empty_pop() {
+        let mut rb = RingBuffer::<usize>::default();
+        let val = rb.pop_front();
+        assert_eq!(val, None);
+        let val = rb.pop_back();
+        assert_eq!(val, None);
+    }
+
+    #[test]
+    fn over_pop() {
+        let mut rb = RingBuffer::<usize>::default();
+        rb.push_back(0);
+        rb.push_back(1);
+
+        let val = rb.pop_back();
+        assert_eq!(val, Some(1));
+        let val = rb.pop_back();
+        assert_eq!(val, Some(0));
+
+        let should_be_none = rb.pop_back();
+        assert_eq!(should_be_none, None);
+        let should_be_none = rb.pop_front();
+        assert_eq!(should_be_none, None);
+
+        rb.push_back(10);
+        let final_val = rb.pop_back();
+        assert_eq!(final_val, Some(10));
+    }
+
+    #[test]
+    fn over_push_back() {
+        let mut rb = RingBuffer::<usize>::new(3);
+        rb.push_back(0);
+        rb.push_back(1);
+        rb.push_back(2);
+
+        rb.push_back(3);
+        rb.push_back(4);
+        rb.push_back(5);
+        let result = rb.get_values();
+        assert_eq!(rb.capacity, 3);
+        assert_eq!(rb.size, 3);
+
+        assert_eq!(rb.head, 0);
+        assert_eq!(rb.tail, 0);
+        assert_eq!(result, vec![3, 4, 5]);
+    }
+
+    #[test]
+    fn head_overwritten() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE * 2) {
+            rb.push_back(i);
+            if i >= DEFAULT_RINGBUF_SIZE {
+                expected.push(i);
+            }
+        }
+
+        let result = rb.get_values();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn stack_sim() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE / 2) {
+            rb.push_back(i);
+            expected.push(i);
+        }
+
+        for i in 0..(DEFAULT_RINGBUF_SIZE / 4) {
+            let val = rb.pop_front();
+            assert_eq!(val, Some(i));
+            assert_eq!(rb.get_values(), expected[i+1..]);
+        }
+    }
+
+    #[test]
+    fn overflow_stack_sim() {
+        let mut rb = RingBuffer::<usize>::default();
+        let mut expected = Vec::<usize>::new();
+        for i in 0..(DEFAULT_RINGBUF_SIZE * 2) {
+            rb.push_back(i);
+            expected.push(i);
+        }
+
+        let expected = &expected[DEFAULT_RINGBUF_SIZE..];
+
+        for i in 0..DEFAULT_RINGBUF_SIZE {
+            let val = rb.pop_front();
+            assert_eq!(val, Some(DEFAULT_RINGBUF_SIZE + i));
+            assert_eq!(rb.get_values(), expected[i+1..]);
+        }
+    }
+}
